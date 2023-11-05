895109a88b6f24f5b635015105337685dcb51db7
KilluaSSR <xiaotian2001@outlook.com>
2023-11-05 22:48:32 +0800

    Lofter-dl-添加系统判断

--- src/lofter-dl.kt
+++ src/lofter-dl.kt
@@ -1,36 +1,61 @@
-import java.io.BufferedReader
-import java.io.FileOutputStream
-import java.io.InputStream
-import java.io.InputStreamReader
+import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.*
 import java.util.regex.Pattern
+import kotlin.collections.HashSet
 import kotlin.concurrent.thread
 
 fun main() {
     val link = readLine()
     val map = System.getenv()
     val userName = map["USER"]
-    if(!link.isNullOrBlank()){
-        loadData(link.toString(),userName)
+    val mac_path = "/Users/${userName}/Pictures/lofter爬虫/"
+    val windows_path = "%SystemDrive%\\Users\\%USERPROFILE%\\Downloads\\lofter爬虫"
+    val path = when{
+        checkOS() == OS.WINDOWS -> windows_path
+        checkOS() == OS.MAC ->mac_path
+        else -> ""
     }
+    if(!checkDir(userName,path)){File(path).mkdir()}
+    if(!link.isNullOrBlank()){
+        loadData(link.toString(),userName,path)
+    }
+    for(items in map){
+        println(items)}
 
 }
+enum class OS {
+    WINDOWS, MAC
+}
+fun checkOS():OS?{
+    val os = System.getProperty("os.name").lowercase(Locale.getDefault())
+    return when {
+        os.contains("win") -> {
+            OS.WINDOWS
+        }
+        os.contains("mac") -> {
+            OS.MAC
+        }
+        else -> null
+    }
+}
+fun checkDir(userName: String?,path:String)= File(path).isDirectory
 
-
-fun loadData(URL:String,userName:String?){
+fun loadData(URL:String,userName:String?,path: String){
     val resultSet = HashSet<String>()
     thread {
         val url = URL(URL)
         val connection = url.openConnection() as HttpURLConnection
         try {
             val response = StringBuilder()
-            connection.setRequestProperty("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
-            connection.setRequestProperty("Referer",URL)
-            connection.setRequestProperty("Origin",URL)
-            connection.requestMethod = "GET"
-            connection.connectTimeout = 8000
-            connection.readTimeout = 8000
+            connection.apply {
+                setRequestProperty("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+                setRequestProperty("Referer",URL)
+                requestMethod = "GET"
+                connectTimeout = 8000
+                readTimeout = 8000
+            }
             val input = connection.inputStream
             val reader = BufferedReader(InputStreamReader(input))
             reader.apply {
@@ -44,7 +69,7 @@ fun loadData(URL:String,userName:String?){
                 resultSet.add(matcher.group(1))
             }
             for(items in resultSet){
-                download(items,userName)
+                download(items,userName,path)
             }
 
         }catch (e:Exception){
@@ -54,22 +79,23 @@ fun loadData(URL:String,userName:String?){
         }
     }
 }
-fun download(imageURL:String,userName: String?) {
+fun download(imageURL:String,userName: String?,path: String) {
     val imageUrl = imageURL
     val url = URL(imageUrl)
     thread {
         val connection = url.openConnection() as HttpURLConnection
         try {
-            connection.requestMethod = "GET"
-            connection.setRequestProperty(
-                "User-Agent",
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
-            )
-            connection.setRequestProperty("Referer", imageURL)
-            connection.setRequestProperty("Origin", imageURL)
+            connection.apply {
+                setRequestProperty("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+                setRequestProperty("Referer",imageURL)
+                setRequestProperty("Origin", imageURL)
+                requestMethod = "GET"
+                connectTimeout = 8000
+                readTimeout = 8000
+            }
             val inputStream: InputStream = connection.inputStream
             val result: Regex = Regex(pattern = "/([^/]+)\$")
-            val outputPath = "/Users/${userName}/Pictures/lofter爬虫/${result.find(imageURL)?.value}"
+            val outputPath = "${path}"+"${result.find(imageURL)?.value}"
             val outputStream = FileOutputStream(outputPath)
             val buffer = ByteArray(4096)
             var bytesRead: Int
